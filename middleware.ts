def3261878561c14909c75845fbcc6bd1f0c5e08
sushma-elshaddaiCloud <eventfacts.86@gmail.com>
2025-07-15 20:47:48 +0530

    Fix Clerk middleware protect method

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,9 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  if (isProtectedRoute(req)) {
+    auth.protect();
+  }
 });
 
 export const config = {
